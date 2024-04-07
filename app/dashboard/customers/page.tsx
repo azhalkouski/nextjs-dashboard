dc4fd7b77fc10ab4c9c9e89c41901344dfb5cc3b
Artsiom Zhalkouski <azhalkouski@gmail.com>
2024-04-07 18:01:58 +0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log('render Customers Page');
+  return <p>Customers Page</p>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log('render Dashboard Page');
+  return <p>Dashboard Page</p>;
+}
